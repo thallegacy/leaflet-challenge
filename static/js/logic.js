9e079b4d7f57a157fb11bb80e50e6448c17c8fbb
thallegacy <thallegacy@users.noreply.github.com>
2021-03-13 07:55:57 -0500

    add color function

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,12 @@
-// Creating map object
-var myMap = L.map("mapid", {
-    center: [37.0902, -95.7129],
-    zoom: 5
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 6,
   });
   
-  // Adding tile layer
+  // Adding a tile layer (the background map image) to our map
+  // We use the addTo method to add objects to our map
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -14,3 +16,22 @@ var myMap = L.map("mapid", {
     accessToken: API_KEY
   }).addTo(myMap);
   
+  // Use this link to get the geojson data.
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+// Function that will determine the color of a cirle based on the magnitude
+function chooseColor(magnitude) {
+    if (magnitude > 5) {
+        return '#ee6c6e'
+    } else if (magnitude > 4) {
+        return '#eea770'
+    } else if (magnitude > 3) {
+        return '#f2b957'
+    } else if (magnitude > 2) {
+        return '#f2db5a'
+    } else if (magnitude > 1) {
+        return '#e2f15b'
+    } else {
+        return '#b8f15a'
+    }
+};
